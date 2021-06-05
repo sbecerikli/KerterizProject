3584b68d36a4d754b15292b3cf8bfda7827632d7
seyfullahbecerikli <sbecerikli@gmail.com>
2021-06-05 22:52:43 +0300

    First commit

--- src/main/java/SensorOperations.java
+++ src/main/java/SensorOperations.java
@@ -12,7 +12,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 
-public class KafkaProducerSample
+public class SensorOperations
 {
     public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
 
